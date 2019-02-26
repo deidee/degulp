4f539f5000a0b6ff8e900bf3e3004b7efd1e807d
acj <gitlab@acjs.net>
2019-02-26 09:25:03 +0100

    Added gulp-uglify.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,16 +2,26 @@
 'use strict';
 
 var gulp = require('gulp');
+var pipeline = require('readable-stream').pipeline;
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
 sass.compiler = require('node-sass');
 
+gulp.task('compress', function () {
+    return pipeline(
+        gulp.src('src/js/*.js'),
+        uglify(),
+        gulp.dest('dist/js')
+    );
+});
+
 gulp.task('sass', function () {
-    return gulp.src('./sass/**/*.scss')
+    return gulp.src('./src/sass/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+    gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
 });
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ![deGulp](https://deidee.com/logo.png?str=deGulp)
+# ![deGulp](https://deidee.com/logo.png?str=degulp)
+
+Een template/test met [gulp.js](https://gulpjs.com/) voor [deidee](https://deidee.nl/).
